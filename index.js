e34616555c043268fb5126c7d884d2f677074b38
GWnbsp <2438205355@qq.com>
2024-11-28 22:45:56 +0800

    增加笔记

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+const b = class Persion {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+};
+console.log(0 /* a.Down */);
---
+++ index.ts
@@ -0,0 +1,17 @@
+const b = class Persion {
+    name: string
+    age: number
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    } 
+}
+
+const enum a {
+    Down
+}
+console.log(a.Down);
+
+
+
+
